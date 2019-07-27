2468b3258891c3a32c001a6c3df7e53282d9cc44
bracketcamp <mohammadhosein.ghad@gmail.com>
2019-07-27 17:44:34 +0430

    PlayerController script

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 100f;
+    public float jumpSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    private SpriteRenderer sr;
+
+    private bool isGrounded = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        rb.AddForce(new Vector2(h * speed * Time.deltaTime, 0f));
+
+        if (h == 1)
+            sr.flipX = false;
+        else if (h == -1)
+            sr.flipX = true;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector2(0f, jumpSpeed);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
+}
