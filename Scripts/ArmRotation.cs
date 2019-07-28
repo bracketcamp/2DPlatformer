a202e48e2e0bd1eb1e55e26e5c67cc97387e94b0
bracketcamp <mohammadhosein.ghad@gmail.com>
2019-07-28 14:32:57 +0430

    Scripts

---
+++ Scripts/ArmRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ArmRotation : MonoBehaviour {
+
+    public int offset;
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        mousePos.Normalize();
+
+        float rotZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+    }
+
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,18 +3,14 @@
 public class PlayerController : MonoBehaviour {
 
     public float speed = 100f;
-    public float jumpSpeed = 5f;
 
     private Rigidbody2D rb;
-
     private SpriteRenderer sr;
-
-    private bool isGrounded = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponent<SpriteRenderer>();
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
 
     void Update()
@@ -27,26 +23,6 @@ public class PlayerController : MonoBehaviour {
             sr.flipX = false;
         else if (h == -1)
             sr.flipX = true;
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.velocity = new Vector2(0f, jumpSpeed);
-        }
     }
 
-    void OnCollisionStay2D(Collision2D col)
-    {
-        if (col.collider.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.collider.tag == "Ground")
-        {
-            isGrounded = false;
-        }
-    }
 }
