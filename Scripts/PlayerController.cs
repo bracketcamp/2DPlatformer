9a327636458b5ec5683a31d8a2a73e956a8fc465
bracketcamp <mohammadhosein.ghad@gmail.com>
2019-08-04 14:41:54 +0430

    playerJumpAndCameraFollow

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,9 +3,12 @@
 public class PlayerController : MonoBehaviour {
 
     public float speed = 100f;
+    public float jumpSpeed = 8f;
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+
+    private bool isGrounded = true;
 
     void Start()
     {
@@ -23,6 +26,27 @@ public class PlayerController : MonoBehaviour {
             sr.flipX = false;
         else if (h == -1)
             sr.flipX = true;
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+            rb.velocity = jumpVelocity;
+        }
     }
 
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.collider.tag == "Ground")
+        {
+            isGrounded = false;
+        }
+    }
 }
---
+++ Scripts/PlayerFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour {
+
+    public Transform player;
+
+    public float minY = -1f;
+
+    private Vector3 lastPos;
+
+    void LateUpdate()
+    {
+        if (lastPos == null)
+            lastPos = transform.position;
+        if(player.position != lastPos)
+        {
+            transform.position = new Vector3(player.position.x, Mathf.Clamp(player.position.y, minY, Mathf.Infinity), -10f);
+            lastPos = transform.position;
+        }
+    }
+
+}
