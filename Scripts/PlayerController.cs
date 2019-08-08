e7672fa8061f6aae9d62d99acae5c6f9076fe206
bracketcamp <mohammadhosein.ghad@gmail.com>
2019-08-08 18:11:27 +0430

    PlayerAnimation

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+    private Animator anim;
 
     private bool isGrounded = true;
 
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour {
     {
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -27,18 +29,25 @@ public class PlayerController : MonoBehaviour {
         else if (h == -1)
             sr.flipX = true;
 
+        if (h != 0)
+            anim.SetBool("Run", true);
+        else
+            anim.SetBool("Run", false);
+
         if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
             rb.velocity = jumpVelocity;
+            anim.SetBool("Jump", true);
         }
     }
 
-    void OnCollisionStay2D(Collision2D col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         if (col.collider.tag == "Ground")
         {
             isGrounded = true;
+            anim.SetBool("Jump", false);
         }
     }
 
